715c9ea1b52563bfe3a11444d0ae0136f2f5e3d3
TrevorClute <121002446+TrevorClute@users.noreply.github.com>
2023-11-02 13:11:57 -0400

    Initial Commit

---
+++ aiCodeQuiz/aiCodeQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  aiCodeQuizApp.swift
+//  aiCodeQuiz
+//
+//  Created by Trevor Clute on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct aiCodeQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
